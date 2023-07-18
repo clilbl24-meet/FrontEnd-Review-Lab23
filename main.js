d6cc362635751c31d35ffcd74e652e1c66731360
clilbl24-meet <clilblaukopf@gmail.com>
2023-07-18 08:58:13 +0300

    h

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function changeBackgroundColor(color){
-	document.body.style.background = color;
+function changeBackgroundColor(){
+	if (document.getElementsByTagName("body")[0].style.backgroundColor == "purple"){
+		console.log("Changed to pink");
+		document.getElementsByTagName("body")[0].style.backgroundColor = "pink";
+	}
+	else{
+		if(document.getElementsByTagName("body")[0].style.backgroundColor == "pink"){
+			console.log("Changed to purple");
+			document.getElementsByTagName("body")[0].style.backgroundColor = "purple";
+		}
+	}
 }
+
+
+document.getElementsByTagName("body")[0].style.backgroundColor = "purple";
